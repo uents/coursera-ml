6d61a0e506dfd04e0afc45807e9efec7e3932696
uents <uents@ymail.com>
2016-08-18 01:24:55 +0900

    week02: add the notebook of linear regression with multiple variables

---
+++ week02/linear_regression_multi/linear_regression_multi.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+# Computing the cost J
+def compute_cost_multi(X, y, theta):
+    m = len(y)
+    J = 1/(2 * m) * np.sum(np.power(np.dot(X, theta) - y, 2))
+    return J
+
+# Gradient Descent
+def gradient_descent_multi(X, y, theta, alpha, num_iters):
+    m = len(y)
+    J_history = []
+    for iter in range(num_iters):
+        theta = theta - alpha * (1/m) * np.dot(X.T, (np.dot(X, theta) - y))
+        J = compute_cost_multi(X, y, theta)
+        J_history.append(J)
+    return theta, J_history
