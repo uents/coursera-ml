96c37a333e01fba090ef764db1d89216e57fb5df
uents <uents@ymail.com>
2016-08-19 14:58:55 +0900

    week03: clean code of logistic regression

--- week03/logistic_regression/logistic_regression.py
+++ week03/logistic_regression/logistic_regression.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from scipy import optimize
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
@@ -20,8 +21,8 @@ def compute_cost(theta, X, y):
     """
     m = len(y)
     h = sigmoid(np.dot(X, theta))
-    J = 1/m * sum(-y * safe_log(h) - (1-y) * safe_log(1-h))
-    return J[0]
+    J = 1/m * np.sum(-y * safe_log(h) - (1-y) * safe_log(1-h))
+    return J
 
 def compute_grad(theta, X, y):
     """
@@ -39,5 +40,17 @@ def compute_grad(theta, X, y):
     grad = 1/m * np.dot(X.T, (h - y))
     return grad
 
-
-
+def compute_theta(initial_theta, X, y):
+    def _compute_cost(theta, X, y):
+        t = theta.reshape(len(theta), 1)
+        J = compute_cost(t, X, y)
+        return J
+    
+    def _compute_grad(theta, X, y):
+        t = theta.reshape(len(theta), 1)
+        grad = compute_grad(t, X, y)
+        return grad.flatten()
+    
+    theta = optimize.fmin_cg(_compute_cost, fprime=_compute_grad,
+                             x0=initial_theta, args=(X, y))
+    return theta
