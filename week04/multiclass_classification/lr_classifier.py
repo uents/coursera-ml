11fc5bb9401b9439ea9f1b702f4e6eb67af2aa13
uents <uents@ymail.com>
2016-08-27 20:14:37 +0900

    week04 add the notebook of multi-class classification

---
+++ week04/multiclass_classification/lr_classifier.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from scipy import optimize
+
+import sys,os
+sys.path.append(
+    os.path.dirname(os.path.abspath(__file__))
+    + '/../../week03/regularized_logistic_regression')
+import regularized_logistic_regression as lr
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def optimize_thetas(X, y, num_labels, lmd):
+    """
+    Optimize the parameters
+    
+    Parameters
+    ----------
+    X : array-like, shape (n_examples, n_dim)
+        input dataset
+    y : array-like, shape (n_examples, 1)
+        output label dataset
+    num_labels : int
+        numeber of classification labels
+    lmd : float
+        regularization coefficient
+    
+    Returns
+    -------
+    thetas : array-like, shape (n_dim, 1)
+        optimized parameters of each class
+    """
+    assert(X.shape[0] == y.shape[0])
+
+    def _compute_cost(theta, X, y, lmd):
+        _t = theta.reshape(len(theta), 1)
+        _y = y.reshape(len(y), 1)
+        J = lr.compute_cost_reg(_t, X, _y, lmd)
+        return J
+
+    def _compute_grad(theta, X, y, lmd):
+        _t = theta.reshape(len(theta), 1)
+        _y = y.reshape(len(y), 1)
+        grad = lr.compute_grad_reg(_t, X, _y, lmd)
+        return grad.ravel()
+
+    m = X.shape[0]
+    n = X.shape[1]
+
+    thetas = np.zeros((num_labels, n + 1))
+    X = np.c_[np.ones((m, 1)), X]
+    initial_theta = np.zeros((n + 1, 1))
+
+    for label in range(1, num_labels+1):
+        _y = np.array([1 if c == label else 0 for c in y])
+        print('training : label', label, '...')
+        res = optimize.minimize(
+            method='BFGS', fun=_compute_cost, jac=_compute_grad,
+            x0=initial_theta, args=(X, _y, lmd), options={'maxiter': 50})
+        thetas[label-1] = res.x
+
+    return thetas
+
+
+def predict(thetas, X):
+    """
+    predict classification
+    
+    Parameters
+    ----------
+    thetas : array-like, shape (n_classes, n_dim+1)
+        parameters
+    X : array-like, shape (n_examples, n_dim)
+        input dataset
+    
+    Returns
+    -------
+    ypreds : array-like, shape (n_examples, 1)
+        predicted classes
+    """
+    X = np.c_[np.ones((X.shape[0], 1)), X]
+    assert(X.shape[1] == thetas.shape[1])
+
+    pred_values = sigmoid((np.dot(X, thetas.T)))
+    pred_classes = [(np.argmax(p) + 1) for p in pred_values]
+    return np.array(pred_classes).reshape(len(pred_classes), 1)
+
+def compute_train_accuracy(ypreds, y):
+    """
+    Compute the training accuracy
+    
+    Parameters
+    ----------
+    ypreds : array-like, shape (n_examples, 1)
+        predicted dataset
+    y : array-like, shape (n_examples, 1)
+        correct dataset
+    
+    Returns
+    -------
+    accuracy : float
+        training accuracy
+    """
+    assert(ypreds.shape == y.shape)
+    return np.mean(ypreds.ravel() == y.ravel()) * 100.
---
+++ week04/multiclass_classification/nn_classifier.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def predict(theta1, theta2, X):
+    """
+    predict classification
+    
+    Parameters
+    ----------
+    theta1 : array-like, shape (n_hidden_unit, n_dim+1)
+        parameters of input layer
+    theta2 : array-like, shape (n_classes, n_hidden_unit+1)
+        parameters of hidden layer
+    X : array-like, shape (n_examples, n_dim)
+        input dataset
+    
+    Returns
+    -------
+    ypreds : array-like, shape (n_examples, 1)
+        predicted classes
+    """
+    assert(X.shape[1] + 1 == theta1.shape[1])
+    assert(theta1.shape[0] + 1 == theta2.shape[1])
+
+    m = X.shape[0]
+    a1 = np.c_[np.ones((m, 1)), X]
+    z2 = sigmoid(np.dot(a1, theta1.T))
+    a2 = np.c_[np.ones((m, 1)), z2]
+    z3 = sigmoid(np.dot(a2, theta2.T))
+
+    pred_classes = [(np.argmax(p) + 1) for p in z3]
+    return np.array(pred_classes).reshape(len(pred_classes), 1)
+
+def compute_train_accuracy(ypreds, y):
+    """
+    Compute the training accuracy
+    
+    Parameters
+    ----------
+    ypreds : array-like, shape (n_examples, 1)
+        predicted dataset
+    y : array-like, shape (n_examples, 1)
+        correct dataset
+    
+    Returns
+    -------
+    accuracy : float
+        training accuracy
+    """
+    assert(ypreds.shape == y.shape)
+    return np.mean(ypreds.ravel() == y.ravel()) * 100.
