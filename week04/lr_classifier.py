54152f18499fcd26e2379edc874ea7350f8fcff9
uents <uents@ymail.com>
2016-09-13 09:17:37 +0900

    week04: refactoring

--- week04/lr_classifier.py
+++ week04/lr_classifier.py
@@ -31,38 +31,43 @@ def optimize_params(X, y, num_labels, lmd):
     """
     assert(X.shape[0] == y.shape[0])
 
-    def _cost_function(theta, X, y, lmd, j_hist):
+    def _cost_function(theta, X, y, lmd):
+        nonlocal J
         t = theta.reshape(-1, 1)
         J, D = cost_function_reg(t, X, y, lmd)
-        j_hist.append(J)
         return J, D.ravel()
 
-    m, n = X.shape
+    def _callback(theta):
+        nonlocal J_history, J, label
+        J_history.append(J)
+
     num_labels = len(np.unique(y))
+    m, n = X.shape
 
     X = np.c_[np.ones((m, 1)), X]
     thetas = np.zeros((num_labels, n + 1))
     initial_theta = np.zeros((n + 1, 1))
-    J_history = {}
 
+    J_hists = {};
     for label in np.unique(y):
         _y = np.array([1 if l == label else 0 for l in y]).reshape(-1, 1)
-        j_hist = []
+        J_history = []; J = 0
+
         res = scipy.optimize.minimize(
             method='BFGS', fun=_cost_function, jac=True,
-            x0=initial_theta, args=(X, _y, lmd, j_hist),
-            options={'maxiter': 50})
+            x0=initial_theta, args=(X, _y, lmd),
+            options={'maxiter': 50}, callback=_callback)
 
+        print('training : label=' + str(label) + ' J=' + str(J_history[-1]))
         thetas[label] = res.x
-        print('... training : label=' + str(label) + ' J=' + str(j_hist[-1]))
-        J_history.update({str(label): j_hist})
+        J_hists.update({str(label): J_history})
     
-    return thetas, J_history
+    return thetas, J_hists
 
 
 def predict(thetas, X):
     """
-    predict classification
+    Predict classification
     
     Parameters
     ----------
@@ -73,14 +78,14 @@ def predict(thetas, X):
     
     Returns
     -------
-    ypreds : array-like, shape (n_examples, 1)
-        prediction classes and values
+    ypreds : list (n_examples)
+        prediction class and values
     """
     X = np.c_[np.ones((X.shape[0], 1)), X]
     assert(X.shape[1] == thetas.shape[1])
 
     values = sigmoid((np.dot(X, thetas.T)))
-    preds = [{'class': np.argmax(p), 'value': np.max(p)} for p in values]
+    preds = [{'class': np.argmax(p), 'values': p} for p in values]
     return preds
 
 def compute_train_accuracy(ypreds, y):
